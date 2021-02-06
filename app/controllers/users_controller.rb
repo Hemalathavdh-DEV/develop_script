296810b80c5ba16a286d0119f9930b29b34e8ebc
Lathavdh <hemalathavdh@gmail.com>
2021-02-06 12:55:36 +0530

    Initial  Commit and table changes in the new branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
 private
 	def user_strong_params
-		params.require(:user).permit!(
+		params.require(:user).permit(
 		:user_name,
 		:email,
 		:encrypted_password,
